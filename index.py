c53f7398ec9cb0ad120c9f4d920fbf1405166138
Sebastian Ong <78428559+SebassNoob@users.noreply.github.com>
2022-05-02 14:53:53 +0000

    404, page change animation, remove contents table

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from threading import Thread
 app = Flask(__name__)
 
@@ -6,8 +6,17 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/exp_edu')
+def exp_edu():
+  return render_template('exp_edu.html')
 
+@app.route('/404')
+def pg404():
+  return render_template('err404.html')
 
+@app.errorhandler(404)
+def err404(e):
+  return redirect(url_for('pg404'))
 run_app = app.run(host='0.0.0.0',port=1000)
 while True:
   t=Thread(target= run_app).start()
